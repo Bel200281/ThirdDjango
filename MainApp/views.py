787c087c94419c80614c667c1b4ed3221c41946e
Belutkina Elena <belenka@rambler.ru>
2025-05-20 23:57:34 +0300

    First line

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from MainApp.models import Item
-from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
+def home(request):
+    text = """
+    <h1>"Изучаем Django"</h1>
+    <strong>Автор</strong>:<i>Иванов И.П.</i>
+
+    """
+    return HttpResponse(text)
